3f72b70f485fd26c1198e4689d2a940945d44355
Nina <nina.cakija@gmail.com>
2024-11-09 17:42:35 +0100

    Dodan prvo dio Google OAuth-a

--- IzvorniKod/Apartmeet/Apartmeet.Api/Program.cs
+++ IzvorniKod/Apartmeet/Apartmeet.Api/Program.cs
@@ -1,10 +1,21 @@
 using Apartmeet.Api.Data;
 using Apartmeet.Api.Endpoints;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration.GetConnectionString("Apartmeet");
 builder.Services.AddSqlite<ApartmeetContext>(connString);
+
+var cid = builder!.Configuration["Authentication:Google:ClientId"];
+var csec = builder!.Configuration["Authentication:Google:ClientSecret"];
+
+builder.Services.AddAuthentication()
+   .AddGoogle(googleOptions =>
+    {
+        googleOptions.ClientId = builder!.Configuration["Authentication:Google:ClientId"];
+        googleOptions.ClientSecret = builder!.Configuration["Authentication:Google:ClientSecret"];
+    });
 
 var app = builder.Build();
 

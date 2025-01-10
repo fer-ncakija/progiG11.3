9a5de0cc9773546242e1bbb51a6ecb2df5509513
doroteapozega <dorotea.pozega@fer.unizg.hr>
2025-01-10 13:08:54 +0100

    prosirena forma za dodavanje sastanaka

--- ApartMeet - front/src/components/DodajSastanak.js
+++ ApartMeet - front/src/components/DodajSastanak.js
@@ -3,24 +3,92 @@ import "./DodajSastanak.css";
 import {useState} from 'react'
 import { Link } from 'react-router-dom'
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 function DodajSastanak(){
+
+    const [meetForm, setMeetForm] = React.useState({
+        naslov: "",
+        sazetak: "",
+        vrijeme: "",
+        mjesto: "",
+    });
+
+    // provjera je li forma za prijavu ispravna
+    function isValid() {
+        const { naslov, sazetak, vrijeme, mjesto } = meetForm;
+        return naslov.length > 0 && sazetak.length > 0 && vrijeme.length > 0 && mjesto.length > 0;
+    }
+
+    // ažurira podatke u formi na temelju korisnikovog unosa
+    function onChange(event) {
+        const { name, value } = event.target;
+        setMeetForm((oldForm) => ({ ...oldForm, [name]: value }));
+    }
+
+    // funkcija za obradu slanja forme
+    function onSubmit(e) {
+        e.preventDefault();
+
+        const data = {
+        naslov: meetForm.naslov,
+        sazetak: meetForm.sazetak,
+        vrijeme: meetForm.vrijeme,
+        mjesto: meetForm.mjesto,
+        };
+        const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        };
+        fetch(`${apiUrl}/meetings`, options);  // mora se dodati ruta za meetings u backendu (mozda ce biti drugacije ime)
+    }
+
+
     return(
-        <div className="box">
-            <form>
-                <div className="inputs">
-                    <div className="input">
+        <div className="meetbox">
+            <form onSubmit={onSubmit}>
+                <div className="meetinputs">
+                    <div className="meetinput">
                         <input
-                            name="naziv"
-                            placeholder="Naziv sastanka"
+                            name="naslov"
+                            placeholder="naslov sastanka"
+                            onChange={onChange}
+                            value={meetForm.naslov}
                         />
                     </div>
-                    <div className="input">
+                    <div className="meetinput">
                         <input
-                            name="datum"
-                            type="date"
-                            placeholder="Datum"
+                            name="sazetak"
+                            placeholder="sazetak"
+                            onChange={onChange}
+                            value={meetForm.sazetak}
                         />
                     </div>
+                    <div className="meetinput">
+                        <input
+                            name="vrijeme"
+                            type="datetime-local"
+                            placeholder="datum i vrijeme"
+                            onChange={onChange}
+                            value={meetForm.vrijeme}
+                        />
+                    </div>
+                    <div className="meetinput">
+                        <input
+                            name="mjesto"
+                            placeholder="mjesto"
+                            onChange={onChange}
+                            value={meetForm.mjesto}
+                        />
+                    </div>
+                </div>
+                <div className="submitmeet">
+                    <button className="meetbutton" type="submit" disabled={!isValid()}>
+                        Dodaj sastanak
+                    </button>
                 </div>
             </form>
         </div>
